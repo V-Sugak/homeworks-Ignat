6c8493e35ce6a8279701301294799e874b9351c3
V-Sugak <vicka.sugak2010@yandex.ru>
2021-10-19 22:41:21 +0300

    homework-1

--- src/p2-homeworks/h1/HW1.tsx
+++ src/p2-homeworks/h1/HW1.tsx
@@ -1,11 +1,13 @@
 import React from 'react'
+import {Message, MessagePropsType} from "./Message";
 
-const messageData = {
+const messageData: MessagePropsType = {
     avatar: 'https://sun9-74.userapi.com/Ph-WiuOtF985il9AvN9JqiCWedmHtSGSSTXrSA/ltEB2Z2-YO4.jpg',
-    name: 'Some Name',
-    message: 'some text',
+    name: 'Artem',
+    message: 'npm start нажимал ?',
     time: '22:00',
 }
+
 
 function HW1() {
     return (
@@ -13,14 +15,12 @@ function HW1() {
             <hr/>
             homeworks 1
 
-            {/*should work (должно работать)*/}
-
-            {/*<Message*/}
-            {/*    avatar={messageData.avatar}*/}
-            {/*    name={messageData.name}*/}
-            {/*    message={messageData.message}*/}
-            {/*    time={messageData.time}*/}
-            {/*/>*/}
+            <Message
+                avatar={messageData.avatar}
+                name={messageData.name}
+                message={messageData.message}
+                time={messageData.time}
+            />
 
             <hr/>
             {/*для личного творчества, могу проверить*/}

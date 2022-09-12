65b27972ee5689def6303ca7b74b6f5a1c5df00c
V-Sugak <vicka.sugak2010@yandex.ru>
2022-09-12 15:05:19 +0300

    homework-4: SuperInput, SuperButton

--- src/p2-homeworks/h4/common/c1-SuperInputText/SuperInputText.tsx
+++ src/p2-homeworks/h4/common/c1-SuperInputText/SuperInputText.tsx
@@ -39,11 +39,12 @@ const SuperInputText: React.FC<SuperInputTextPropsType> = (
     }
 
     const finalSpanClassName = `${s.error} ${spanClassName ? spanClassName : ''}`
-    const finalInputClassName = `${s.errorInput} ${className}` // need to fix with (?:) and s.superInput
+    const finalInputClassName = `${error ? s.errorInput : s.input} ${className}` // need to fix with (?:) and s.superInput
 
     return (
         <>
             <input
+
                 type={'text'}
                 onChange={onChangeCallback}
                 onKeyPress={onKeyPressCallback}

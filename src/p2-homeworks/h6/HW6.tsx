26bfaeecaa501b49e5cf46bb06b383c2a9bbae5a
V-Sugak <vicka.sugak2010@yandex.ru>
2022-09-15 13:07:45 +0300

    homework-6

--- src/p2-homeworks/h6/HW6.tsx
+++ src/p2-homeworks/h6/HW6.tsx
@@ -2,6 +2,7 @@ import React, {useState} from 'react'
 import SuperEditableSpan from './common/c4-SuperEditableSpan/SuperEditableSpan'
 import SuperButton from '../h4/common/c2-SuperButton/SuperButton'
 import {restoreState, saveState} from './localStorage/localStorage'
+import s from './HW6.module.css'
 
 function HW6() {
     const [value, setValue] = useState<string>('')
@@ -10,7 +11,8 @@ function HW6() {
         saveState<string>('editable-span-value', value)
     }
     const restore = () => {
-        // setValue()
+        const defaultValue = restoreState('editable-span-value', '')
+        setValue(defaultValue)
     }
 
     return (
@@ -19,15 +21,22 @@ function HW6() {
             homeworks 6
 
             {/*should work (должно работать)*/}
-            <div>
-                <SuperEditableSpan
-                    value={value}
-                    onChangeText={setValue}
-                    spanProps={{children: value ? undefined : 'enter text...'}}
-                />
+            <div className={s.block}>
+                <div className={s.spanBlock}>
+                    <SuperEditableSpan
+                        value={value}
+                        onChangeText={setValue}
+                        spanProps={{children: value ? undefined : 'enter text...'}}
+                    />
+                    <img className={s.img}
+                         src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTy7KighJf74XBeg9alnuRn2icc3Q8M1kA5DQ&usqp=CAU"
+                         alt="image"/>
+                </div>
+                <div>
+                    <SuperButton onClick={save}>save</SuperButton>
+                    <SuperButton onClick={restore}>restore</SuperButton>
+                </div>
             </div>
-            <SuperButton onClick={save}>save</SuperButton>
-            <SuperButton onClick={restore}>restore</SuperButton>
 
             <hr/>
             {/*для личного творчества, могу проверить*/}

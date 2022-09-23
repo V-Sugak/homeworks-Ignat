0f6de7449a8e9bdf4c5816ff07b6d10c6a9b0c5e
V-Sugak <vicka.sugak2010@yandex.ru>
2022-09-23 12:17:39 +0300

    homework-11

--- src/p2-homeworks/h11/HW11.tsx
+++ src/p2-homeworks/h11/HW11.tsx
@@ -1,10 +1,21 @@
 import React, {useState} from 'react'
 import SuperRange from './common/c7-SuperRange/SuperRange'
 import SuperDoubleRange from './common/c8-SuperDoubleRange/SuperDoubleRange'
+import s from './HW11.module.css'
 
 function HW11() {
     const [value1, setValue1] = useState(0)
     const [value2, setValue2] = useState(100)
+
+    const onChangeDoubleRange = (value: number | number[]) => {
+        if (Array.isArray(value)) {
+            setValue1(value[0])
+            setValue2(value[1])
+        } else {
+            setValue1(value1)
+        }
+
+    }
 
     return (
         <div>
@@ -15,16 +26,20 @@ function HW11() {
             <div>
                 <span>{value1}</span>
                 <SuperRange
+                    value={value1}
+                    onChangeRange={setValue1}
                     // сделать так чтоб value1 изменялось
                 />
             </div>
 
-            <div>
-                <span>{value1}</span>
+            <div className={s.doubleRange}>
+                <span className={s.span}>{value1}</span>
                 <SuperDoubleRange
                     // сделать так чтоб value1 и value2 изменялось
+                    value={[value1, value2]}
+                    onChangeRange={onChangeDoubleRange}
                 />
-                <span>{value2}</span>
+                <span className={s.span}>{value2}</span>
             </div>
 
             <hr/>
--- src/p2-homeworks/h11/common/c8-SuperDoubleRange/SuperDoubleRange.tsx
+++ src/p2-homeworks/h11/common/c8-SuperDoubleRange/SuperDoubleRange.tsx
@@ -1,7 +1,9 @@
 import React from 'react'
+import {Box, Slider} from "@mui/material";
+import s from './SuperDoubleRange.module.css'
 
 type SuperDoubleRangePropsType = {
-    onChangeRange?: (value: [number, number]) => void
+    onChangeRange: ( value:  number | number[]) => void
     value?: [number, number]
     // min, max, step, disable, ...
 }
@@ -14,11 +16,37 @@ const SuperDoubleRange: React.FC<SuperDoubleRangePropsType> = (
 ) => {
     // сделать самому, можно подключать библиотеки
 
+    function valuetext(value: number) {
+        return `${value}°C`;
+    }
+
+    // const [value, setValue] = React.useState<number[]>([20, 37]);
+
+    const onChangeCallback = (event: Event, newValue: number | number[]) => {
+        onChangeRange(newValue);
+    };
+
     return (
-        <>
-            DoubleRange
-        </>
-    )
+        <Box sx={{width: 200}}>
+            <Slider
+                size="small"
+                color="secondary"
+                getAriaLabel={() => 'Temperature range'}
+                value={value}
+                onChange={onChangeCallback}
+                valueLabelDisplay="auto"
+                getAriaValueText={valuetext}
+            />
+        </Box>
+    );
 }
 
 export default SuperDoubleRange
+
+
+
+
+
+
+
+
